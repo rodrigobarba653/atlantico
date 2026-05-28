8ba4f407ee8467556bc82e5616386bb32827d154
rodrigobarba653 <rodrigobarba653@gmail.com>
2026-05-28 13:20:19 -0600

    MQI meta pixel

--- src/app/me-quiero-ir/page.tsx
+++ src/app/me-quiero-ir/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { SongPage, type Song } from "@/components/SongPage";
+
+const META_PIXEL_ID = "952786404211204";
 
 const meQuieroIr: Song = {
   title: "Me Quiero Ir",
@@ -28,5 +31,33 @@ export const metadata: Metadata = {
 };
 
 export default function MeQuieroIrPage() {
-  return <SongPage song={meQuieroIr} theme="light" />;
+  return (
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+      <SongPage song={meQuieroIr} theme="light" />
+    </>
+  );
 }
